ea6a86f80db5ecb5b3e559597d4e419f170b359f
auliamnaufal <auliamnaufal@gmail.com>
2023-08-18 14:31:47 +0700

    Feat: Add taskform component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,68 @@
-import React from "react";
+import {
+  DefaultPalette,
+  IStackItemStyles,
+  IStackStyles,
+  IStackTokens,
+  Stack,
+} from "@fluentui/react";
+import TaskForm from "./components/TaskForm";
+
+const stackItemStyles: IStackItemStyles = {
+  root: {
+    alignItems: "center",
+    background: DefaultPalette.themeLighter,
+    color: DefaultPalette.black,
+    display: "flex",
+    height: "100vh",
+    justifyContent: "center",
+    overflow: "hidden",
+  },
+};
+const nonShrinkingStackItemStyles: IStackItemStyles = {
+  root: {
+    alignItems: "start",
+    background: DefaultPalette.themeLighter,
+    color: DefaultPalette.black,
+    display: "flex",
+    height: "100vh",
+    justifyContent: "start",
+    overflow: "hidden",
+    width: 500,
+    padding: 20,
+  },
+};
+
+// Tokens definition
+const innerStackTokens: IStackTokens = {
+  childrenGap: 5,
+};
+
+const stackStyles: IStackStyles = {
+  root: {
+    background: DefaultPalette.themeLight,
+    overflow: "hidden",
+  },
+};
 
 const App = () => {
-  return <h1>Hello</h1>;
+  return (
+    <Stack
+      enableScopedSelectors
+      horizontal
+      styles={stackStyles}
+      tokens={innerStackTokens}
+    >
+      <Stack.Item disableShrink styles={nonShrinkingStackItemStyles}>
+        <TaskForm />
+      </Stack.Item>
+      <Stack.Item grow styles={stackItemStyles}>
+        I shrink
+      </Stack.Item>
+      <Stack.Item grow styles={stackItemStyles}>
+        I shrink
+      </Stack.Item>
+    </Stack>
+  );
 };
 
 export default App;
---
+++ frontend/src/components/TaskForm.tsx
@@ -0,0 +1,43 @@
+import {
+  DefaultPalette,
+  IStackStyles,
+  IStackTokens,
+  PrimaryButton,
+  Stack,
+  Text,
+  TextField,
+} from "@fluentui/react";
+
+const innerStackTokens: IStackTokens = {
+  childrenGap: 10,
+};
+
+const stackStyles: IStackStyles = {
+  root: {
+    overflow: "hidden",
+    width: "100%",
+  },
+};
+
+const TaskForm = () => {
+  return (
+    <>
+      <Stack styles={stackStyles} tokens={innerStackTokens}>
+        <Text variant="xxLarge">Create New Task</Text>
+        <form>
+          <Stack styles={stackStyles} tokens={innerStackTokens}>
+            <TextField label="Title" required />
+            <TextField label="Description" multiline rows={3} />
+          </Stack>
+          <PrimaryButton
+            text="Create"
+            allowDisabledFocus
+            style={{ marginTop: 20 }}
+          />
+        </form>
+      </Stack>
+    </>
+  );
+};
+
+export default TaskForm;
